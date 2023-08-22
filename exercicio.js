140694fdaad6d3d3b058e222f6c5884358ca0544
Saritah <142619907+Saramarinho@users.noreply.github.com>
2023-08-22 17:37:13 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,61 @@
+//
+/*
+* SaraMarinho
+*
+*/
+
+/*Exercicios de interpretação de códigos
+* 1. console.log(10, 5).
+* 2.console.log(20, 10, 10).
+...
+*exercicio 3.
+*/
+let horario = prompt("Quantas horas você trabalha por dia?");
+let salario = prompt("Quanto você recebe por dia?");
+alert('você recebe ${salario/horario} por hora');
+/*Exercicios de escrita de código
+*1.
+*/
+let nome;
+let idade = null;
+console.log(typeof nome);
+console.log(typeof idade);
+/*D) Porque eu usei a sintaxe console.log para imprimir o typeof das minhas variáveis.
+
+E)*/
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+console.log(nome);
+console.log(idade);
+/*
+
+F)*/
+console.log(typeof nome);
+console.log(typeof idade);
+/* Notei uma string, um texto, que é o tipo do valor da minha variável.
+
+G)*/
+nome = prompt("Marinna");
+idade = prompt(40);
+console.log("Olá", nome, "você tem", idade, "anos");
+
+/*2.
+*/
+roupagem = prompt("sim!");
+olhos = prompt("não!");
+cabelos = prompt("não!");
+console.log("Sua roupa é azul?", roupagem, "Seus olhos são claros?", olhos, "Seu cabelo é castanho?",cabelos);
+
+/*3.
+*/
+let a = 10;
+let b = 25;
+let c;
+c = a;
+a = b;
+b = c;
+
+console.log("O novo valor de a é", a);
+console.log("O novo valor de b é", b);
+/*
+*/
